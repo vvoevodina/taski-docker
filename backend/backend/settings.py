47d47e891594378da84b211724686b3561023c59
Viktoriya Voevodina <viktech@mail.ru>
2023-06-12 02:14:37 +0300

    changed pep8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,10 +1,7 @@
+import os
 from pathlib import Path
 
-import os
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
@@ -69,7 +66,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-	'PORT': os.getenv('DB_PORT', 5432),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
